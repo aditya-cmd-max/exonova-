c9d2a6277588a4a62f188cb6dc369f9e6f7a2a80
aditya <adityajha1104@gmail.com>
2025-04-09 23:35:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -66,13 +66,13 @@ import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.22.2
 
 // Your Firebase config here
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "XXXXXX",
-  appId: "YOUR_APP_ID"
+ apiKey: "AIzaSyB7F1uRRiGdw489c_18aJeodbxrzsdFb5c",
+  authDomain: "exonova-cd89c.firebaseapp.com",
+  projectId: "exonova-cd89c",
+  storageBucket: "exonova-cd89c.firebasestorage.app",
+  messagingSenderId: "465326262278",
+  appId: "1:465326262278:web:1b7f8f62e1f46c5ca7db87",
+  measurementId: "G-GGG45V0PEF"
 };
 
 const app = initializeApp(firebaseConfig);
