69cde00f463fee9fa758baa664894ba5fde165a8
aditya <adityajha1104@gmail.com>
2025-04-09 22:38:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,13 +58,3 @@ anime({
   easing: 'easeOutCubic'
 });
 
-
-<script>
-  const hamburger = document.querySelector('.hamburger');
-  const navLinks = document.querySelector('.nav-links');
-
-  hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-  });
-</script>
-
