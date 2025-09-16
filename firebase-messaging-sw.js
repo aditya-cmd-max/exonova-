fdfe70decf60b259ed7aed012272c1736ab68806
aditya <adityajha1104@gmail.com>
2025-09-16 21:34:44 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,6 +3,7 @@
 importScripts('https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/11.0.1/firebase-messaging.js');
 
+// Initialize Firebase
 firebase.initializeApp({
   apiKey: "AIzaSyB7F1uRRiGdw489c_18aJeodbxrzsdFb5c",
   authDomain: "exonova-cd89c.firebaseapp.com",
@@ -12,11 +13,13 @@ firebase.initializeApp({
   appId: "1:465326262278:web:1b7f8f62e1f46c5ca7db87"
 });
 
+// Retrieve messaging
 const messaging = firebase.messaging();
 
+// Background message handler
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] Background message received:', payload);
-  
+
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
