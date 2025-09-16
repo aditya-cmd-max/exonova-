c4e103827f66ec1306b3b1f12e6bff1193a939e3
aditya <adityajha1104@gmail.com>
2025-09-16 20:03:28 +0530

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+// Import Firebase scripts
+importScripts("https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/11.0.1/firebase-messaging.js");
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyB7F1uRRiGdw489c_18aJeodbxrzsdFb5c",
+  authDomain: "exonova-cd89c.firebaseapp.com",
+  projectId: "exonova-cd89c",
+  storageBucket: "exonova-cd89c.firebasestorage.app",
+  messagingSenderId: "465326262278",
+  appId: "1:465326262278:web:1b7f8f62e1f46c5ca7db87",
+});
+
+// Retrieve messaging
+const messaging = firebase.messaging();
+
+// Background handler
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: payload.notification.icon
+  });
+});
